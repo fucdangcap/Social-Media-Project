1149e2b7584bb61f0b58f347215a80723b2e01d3
fucdangcap <phuchoang26625@gmail.com>
2025-12-24 13:20:31 +0700

    Sign in Sign up pages

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100 dark:bg-gray-900">
+      <SignIn />
+    </div>
+  );
+}
